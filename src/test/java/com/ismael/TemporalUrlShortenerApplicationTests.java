2ce17b46f996e62b8497b7dbe31e46bdb0ba0d02
mael <maelribeiro59@gmail.com>
2026-05-01 08:14:40 -0400

    chore: initial project setup and gitignore configuration

---
+++ src/test/java/com/ismael/TemporalUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
